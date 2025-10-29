df8df979fa26e0d2145f8ea8fcbf51428bedc732
Mohamed Ali Zbaira <mohamedalizbaira@gmail.com>
2025-10-29 21:47:43 +0100

    add h1 test

--- git.jsx
+++ git.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const git = () => {
   return (
     <div>
-      <h1>git</h1> 
-
+      <h1>git</h1>
+      <h1>test</h1>
       <title>Git</title>
       <h1>first description</h1>
     </div>
